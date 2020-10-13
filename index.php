fe8b9de2a980d3cb8afcf4f456f0c0b7fdfdc548
Lukas Oertel <git@luoe.dev>
2020-10-13 22:43:00 +0200

    Add source code link

--- index.php
+++ index.php
@@ -33,8 +33,10 @@ require_once('config.php');
             Deine Daten werden wegen Coronaverordnungen gebraucht und bis 2 Wochen nach den Veranstaltungen gespeichert.<br>
             Sie werden außerdem, solltest du dich einmal angemeldet haben, lokal in deinem Browser gespeichert, so dass du dich bei weiteren Veranstaltungen schneller anmelden kannst.<br>
             Du kannst diese Daten durch einen Klick aus deinem Browser löschen:<br>
-            <input type="submit" value="Löschen" onclick="localStorage.clear()">
+            <input type="submit" value="Löschen" onclick="localStorage.clear()">            
         </div>
+        <br><br>
+        <a href="https://github.com/fsi-tue/eei">Source Code</a>
     </div>
 </body>
 </html>
