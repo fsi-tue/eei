cdb3e8db27b8adae0a67d43c0a88c04b87df1e16
Lukas Oertel <git@luoe.dev>
2020-10-20 19:28:48 +0200

    Fix missing ">"

--- registration.php
+++ registration.php
@@ -161,7 +161,7 @@
                 <input type="radio" class="form-studiengang" name="studiengang" value="Medizininformatik"> Medizininformatik<br>
                 <input type="radio" class="form-studiengang" name="studiengang" value="Medieninformatik"> Medieninformatik<br>
                 <input type="radio" class="form-studiengang" name="studiengang" value="Machine Learning"> Machine Learning<br>
-                <input type="radio" class="form-studiengang" name="studiengang" value="Kognitionswissenschaft" Kognitionswissenschaft<br><br>
+                <input type="radio" class="form-studiengang" name="studiengang" value="Kognitionswissenschaft"> Kognitionswissenschaft<br><br>
 
                 Abschluss:<br>
                 <input type="radio" class="form-abschluss" name="abschluss" value="Bachelor" required> Bachelor<br>
