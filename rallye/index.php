b97ff8a301bdd079e6763e1a08919601099af115
Indira <46889456+betaRAYRAY@users.noreply.github.com>
2021-06-14 15:48:21 +0200

    Update index.php (#15)
    Registrierug für Stadtrallye aktiviert

--- rallye/index.php
+++ rallye/index.php
@@ -20,14 +20,14 @@ $E = $events[$short]; #select Event
             <h1><?php echo "{$E['name']} - {$CONFIG_TERM}"; ?></h1>
             <h2 class="description icon clock"><?php echo $E['date'];?></h2>
             <h2 class="description icon marker"><?php echo $E['location'];?></h2>
-            <p class="error">Nach momentanen Stand lässt sich noch nicht sagen ob die Wanderung stattfindet! Sollte sie stattfinden verschwindet dieser Textblock und die Anmeldung wird freigeschalten.</p>
+            <!-- <p class="error">Nach momentanen Stand lässt sich noch nicht sagen ob die Wanderung stattfindet! Sollte sie stattfinden verschwindet dieser Textblock und die Anmeldung wird freigeschalten.</p> -->
             Die Anfi Stadtrallye<br>
             Bei der Stadtrallye lassen wir dich und deine Kommilitonen gegeneinander in Teams antreten. 
             Dabei werdet ihr interessante, schöne und verstörende Ecken Tübingens kennen lernen, 
             dabei hoffentlich die Orientierung in eurer neuen Heimat etwas verbessern und Kontakte knüpfen.<br>
             Melde dich mit deinen Daten unten an, um genaue Informationen zu Treffpunkt und deiner Gruppe zu bekommen.
             Diese Daten werden evtl. auch an das Gesundheitsamt weitergegeben. Solltest du damit nicht einverstanden sein oder falsche Daten angeben, kannst du nicht teilnehmen.<br>
-            Du brauchst keine Angst zu haben - wie werden die Gruppengröße so klein wie möglich halten.<br><br>
+            Du brauchst keine Angst zu haben - wir werden die Gruppengröße so klein wie möglich halten.<br><br>
         </div>
         <?php
             echo ($h['info'] == '' ? '' : "<div class='block info'>{$h['info']}</div>");
@@ -38,7 +38,7 @@ $E = $events[$short]; #select Event
                 if ($_SERVER['REQUEST_METHOD'] === 'POST')
                     registerForOfflineEvent($E);
 
-                //showOfflineRegistration($E); //Please comment in if event will happen!
+                showOfflineRegistration($E); //Please comment in if event will happen!
             ?>
         </div>
     </div>
