c8fbeafcf53f91e8905f9c205817bb8d5cd780f3
PhictionalOne <code@phictional.de>
2022-03-17 17:45:53 +0100

    Add sentence about workshop content

--- workshoplatex2/index.php
+++ workshoplatex2/index.php
@@ -27,8 +27,8 @@ $freeSpots = getNumberOfRemainingSpots($events[$short]);
             Ob Übungsblatt oder Abschlussarbeit, im Laufe des Studiums müsst ihr öfters wissenschaftlich und mathematische Texte verfassen.
             Mit LaTeX habt ihr die Möglichkeit, diese schnell und professionell zu erstellen.
             Dieser Workshop bietet euch einen Einstieg in den Umgang mit LaTeX, Overleaf und TexStudio.<br>
-            In diesem Workshop werden fortgeschrittene Themen behandelt. Für Einsteiger empfiehlt es sich zuvor den Latex Basic Kurs zu besuchen.
-            
+            In diesem Workshop werden fortgeschrittene Themen behandelt. Für Einsteiger empfiehlt es sich zuvor den Latex Basic Kurs zu besuchen.<br>
+            Wir werden Befehle, Umgebungen, Schriftstile und Packages behandeln und erstellen.
         </div>
         <?php
             echo ($h['info'] == '' ? '' : "<div class='block info'>{$h['info']}</div>");
