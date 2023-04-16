67e6a2028ba555ae29b94cf7bcf2c08d23d73521
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-04-16 21:12:32 +0200

    Fix path for stylesheet

--- event.php
+++ event.php
@@ -31,7 +31,7 @@ $E = $events[$event_id];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title> <?php echo "{$E['name']} - $CONFIG_TERM"; ?></title>
 </head>
 <body>
