cc3c0bdb0dbb5f893b6eb92bb8edb77872fa60b8
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-04-19 15:26:37 +0200

    Turn off email debugging

--- email.php
+++ email.php
@@ -25,7 +25,7 @@ function sendMailViaPHPMailer($recipient, $subject, $body)
 {
     $mail = new PHPMailer(TRUE);
     try {
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output. */
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output. */
         $mail->isSMTP();
 
         /* https://stackoverflow.com/questions/2491475/phpmailer-character-encoding-issues */
