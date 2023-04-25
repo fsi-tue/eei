2e5f9ad1926d026cbab3eeeac2b7edf4bb8fd16e
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-04-25 21:57:34 +0200

    Fix deprecated usage of file_get_contents

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 error_reporting(E_ERROR);
 date_default_timezone_set("Europe/Berlin");
 
-$FILE_REVISION = "?v=" . file_get_contents(__DIR__ . "/.git/refs/heads/master", NULL, NULL, 0, 40);
+$FILE_REVISION = "?v=" . file_get_contents(__DIR__ . "/.git/refs/heads/master", FALSE, NULL, 0, 40);
 
 # General Information
 $CONFIG_CONTACT = 'fsi@fsi.uni-tuebingen.de';
