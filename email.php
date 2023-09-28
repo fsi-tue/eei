dfe0419ceb4a773db50b7d03d98efc81e15cb72e
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2023-09-28 20:02:20 +0200

    Add attachment and local dev support for email

--- email.php
+++ email.php
@@ -18,34 +18,55 @@ use PHPMailer\PHPMailer\PHPMailer;
  * @param string $recipient
  * @param string $subject
  * @param string $body
+ * @param string $attachment
  *
  * @return bool
  */
-function sendMailViaPHPMailer(string $recipient, string $subject, string $body): bool
+function sendMailViaPHPMailer(string $recipient, string $subject, string $body, string $attachment = '', string $attachment_name = ''): bool
 {
     $mail = new PHPMailer(TRUE);
     try {
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output. */
         $mail->isSMTP();
-
+        
         /* https://stackoverflow.com/questions/2491475/phpmailer-character-encoding-issues */
         $mail->Encoding = 'base64';
         $mail->CharSet = 'UTF-8';
-
+        
         $mail->SMTPAuth = FALSE;
         $mail->SMTPKeepAlive = TRUE;
-	$mail->SMTPSecure = FALSE;
-	$mail->SMTPAutoTLS = FALSE;
+	    $mail->SMTPSecure = FALSE;
+        $mail->SMTPAutoTLS = FALSE;
+
+        // Set variables for local development
+        if (getEnvVar('LOCAL') === 'true') {
+            $mail->SMTPDebug = 1;
+            $mail->SMTPAuth = TRUE;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->SMTPOptions = [
+                'ssl' => [
+                    'verify_peer' => FALSE,
+                    'verify_peer_name' => FALSE,
+                    'allow_self_signed' => TRUE
+                ]
+            ];
+            $mail->Password = getEnvVar('SENDER_PASSWORD');
+            $mail->Username = getEnvVar('SENDER_USERNAME');
+        }
 	
-	$mail->Host = getEnvVar('EMAIL_HOST');
+	    $mail->Host = getEnvVar('EMAIL_HOST');
         $mail->Port = getEnvVar('EMAIL_PORT');
 	
-	$mail->setFrom(getEnvVar('SENDER_EMAIL'), getEnvVar('SENDER_NAME'));
+	    $mail->setFrom(getEnvVar('SENDER_EMAIL'), getEnvVar('SENDER_NAME'));
         $mail->addAddress($recipient);
 
         $mail->isHTML();
         $mail->Subject = $subject;
         $mail->Body = $body;
+        
+        // If an attachment is given, add it to the email. 
+        if ($attachment !== '') {
+            $mail->addStringAttachment($attachment, $attachment_name);
+        }
 
         $mail->send();
         return TRUE;
