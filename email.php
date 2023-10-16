a4e7baded8259f00a1ec50302a20a99acaf3d74e
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2023-10-16 18:04:23 +0200

    Update doc in email

--- email.php
+++ email.php
@@ -19,7 +19,7 @@ use PHPMailer\PHPMailer\PHPMailer;
  * @param string $subject
  * @param string $body
  * @param string $attachment
- *
+ * @param string $attachment_name
  * @return bool
  */
 function sendMailViaPHPMailer(string $recipient, string $subject, string $body, string $attachment = '', string $attachment_name = ''): bool
@@ -27,14 +27,14 @@ function sendMailViaPHPMailer(string $recipient, string $subject, string $body, 
     $mail = new PHPMailer(TRUE);
     try {
         $mail->isSMTP();
-        
+
         /* https://stackoverflow.com/questions/2491475/phpmailer-character-encoding-issues */
         $mail->Encoding = 'base64';
         $mail->CharSet = 'UTF-8';
-        
+
         $mail->SMTPAuth = FALSE;
         $mail->SMTPKeepAlive = TRUE;
-	    $mail->SMTPSecure = FALSE;
+        $mail->SMTPSecure = FALSE;
         $mail->SMTPAutoTLS = FALSE;
 
         // Set variables for local development
@@ -52,17 +52,17 @@ function sendMailViaPHPMailer(string $recipient, string $subject, string $body, 
             $mail->Password = getEnvVar('SENDER_PASSWORD');
             $mail->Username = getEnvVar('SENDER_USERNAME');
         }
-	
-	    $mail->Host = getEnvVar('EMAIL_HOST');
+
+        $mail->Host = getEnvVar('EMAIL_HOST');
         $mail->Port = getEnvVar('EMAIL_PORT');
-	
-	    $mail->setFrom(getEnvVar('SENDER_EMAIL'), getEnvVar('SENDER_NAME'));
+
+        $mail->setFrom(getEnvVar('SENDER_EMAIL'), getEnvVar('SENDER_NAME'));
         $mail->addAddress($recipient);
 
         $mail->isHTML();
         $mail->Subject = $subject;
         $mail->Body = $body;
-        
+
         // If an attachment is given, add it to the email. 
         if ($attachment !== '') {
             $mail->addStringAttachment($attachment, $attachment_name);
