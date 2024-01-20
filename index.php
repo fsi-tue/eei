cbd7a5ffd04b9111e1f4fa5225ca4b1294dfecae
Josef <46693545+am9zZWY@users.noreply.github.com>
2024-01-20 10:24:51 +0100

    Update UI, fix small bugs and improve maintainability (#132)
    * Update UI, fix bugs, and improve maintainability
    
    * Update style for submit button

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ global $CONFIG_TERM, $FILE_REVISION, $events;
 
 // Loads the environment variables from the .env file
 loadEnv('.env');
-function sortByDate($a, $b) {
+function sortByDate($a, $b)
+{
     return ($a['startUTS'] > $b['startUTS']);
 }
 
@@ -20,67 +21,93 @@ usort($events, 'sortByDate');
 
 <!DOCTYPE html>
 <html lang="<?= $localizer->getLang() ?>">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css<?= $FILE_REVISION; ?>">
-    <link rel="stylesheet" href="css/index.css<?= $FILE_REVISION; ?>">
+    <link rel="stylesheet" href="css/icons.css<?= $FILE_REVISION; ?>">
     <title><?= $localizer['title'] ?></title>
 </head>
+
 <body>
-<!-- Icons made by fontawesome.com under CC BY 4.0 License https://fontawesome.com/license/free -->
-<!-- The BBQ-Grill Icon is made by Smashicons from www.flaticon.com -->
-<div id="center">
-    <h1><?= $localizer['title'] ?> - <?= $CONFIG_TERM ?></h1>
+    <!-- Icons made by fontawesome.com under CC BY 4.0 License https://fontawesome.com/license/free -->
+    <!-- The BBQ-Grill Icon is made by Smashicons from www.flaticon.com -->
+    <div id="center">
+        <h1><?= $localizer['title'] ?> - <?= $CONFIG_TERM ?></h1>
 
-    <div class="container">
-        <label>
-            <select id="lang-selection" aria-description="Select language">
-                <option value='de' <?= $localizer->getLang() === 'de' ? 'selected' : '' ?>>🇩🇪 Deutsch</option>
-                <option value="en" <?= $localizer->getLang() === 'en' ? 'selected' : '' ?>>🇬🇧 English</option>
-            </select>
-        </label>
-    </div>
+        <div class="container">
+            <label>
+                <div class="select-container">
+                    <select id="lang-selection" aria-description="Select language">
+                        <option value='de' <?= $localizer->getLang() === 'de' ? 'selected' : '' ?>>🇩🇪 Deutsch</option>
+                        <option value="en" <?= $localizer->getLang() === 'en' ? 'selected' : '' ?>>🇬🇧 English</option>
+                    </select>
+                </div>
+            </label>
+        </div>
 
-    <div class="container">
-        <?php
-        foreach ($events as $E) {
-            if ($E['active']) {
-                // date in first line, time shall go in new line
-                $date = $E['date'];
-                $date = replaceFirstOccurence(" ", ",<br>", $date);
-                ?>
-                <a href="event.php?e=<?= $E['link'] ?>&lang=<?= $localizer->getLang() ?>">
-                    <div class="box icon <?= $E['icon'] ?> <?= markPastEvent($E)?>">
-                        <p class="name"><?= $E['name'] ?></p>
-                        <p class="date"><?= showDateAndTime($E, array('compact' => TRUE)) ?></p>
+        <div class="container">
+            <?php
+            // Sort events by date and if they are active
+            $sorted_events = $events;
+            usort($sorted_events, function ($a, $b) {
+                // sort by active first, then by date
+                $a_status = $a['active'] && time() < $a['startUTS'];
+                $b_status = $b['active'] && time() < $b['startUTS'];
+
+                if ($a_status == $b_status) {
+                    return $a['startUTS'] > $b['startUTS'];
+                } else {
+                    return $a_status < $b_status;
+                }
+            });
+
+            foreach ($sorted_events as $E) {
+                if ($E['active']) {
+                    // date in first line, time shall go in new line
+                    $date = $E['date'];
+                    $date = replaceFirstOccurence(" ", ",<br>", $date);
+            ?>
+                    <a href="event.php?e=<?= $E['link'] ?>&lang=<?= $localizer->getLang() ?>">
+                        <div class="box icon <?= $E['icon'] ?> <?= time() > $E['startUTS'] ? ' past ' : '' ?> float-style">
+                            <p class="name"><?= $E['name'] ?></p>
+                            <p class="date"><?= showDateAndTime($E, array('compact' => TRUE)) ?></p>
+                        </div>
+                    </a>
+            <?php
+                }
+            }
+            ?>
+        </div>
+        <br>
+        <div class="footnotes">
+            <p>
+                <?php echo $localizer['index_savedDataDisclaimer']; ?>
+            </p>
+            <br>
+            <div class="container">
+                <input id="btn-clr" type="submit" value="<?= $localizer['delete'] ?>" onclick="!localStorage.clear() && alert('<?= $localizer['index_deletedData'] ?>')">
+            </div>
+            <div class="container">
+                <a href="https://github.com/fsi-tue/eei">
+                    <div class="link color-border">
+                        Source Code auf Github
                     </div>
                 </a>
-                <?php
-            }
-        }
-        ?>
-    </div>
-    <br>
-    <div class="footnotes">
-        <p>
-        <?php echo $localizer['index_savedDataDisclaimer'];?></p><br>
-        <div class="clear-btn">
-            <input id="btn-clr" type="submit" value="<?= $localizer['delete'] ?>"
-                onclick="!localStorage.clear() && alert('<?= $localizer['index_deletedData'] ?>')">
+            </div>
         </div>
-        <a href="https://github.com/fsi-tue/eei">Source Code auf Github</a>
-    </div>
-    
-</div>
 
-<script>
-    // change language
-    const langSelection = document.getElementById('lang-selection')
-    langSelection.addEventListener('change', () => {
-        this.location.href = `index.php?lang=${langSelection.value}`
-    })
-</script>
+    </div>
+
+    <script>
+        // change language
+        const langSelection = document.getElementById('lang-selection')
+        langSelection.addEventListener('change', () => {
+            this.location.href = `index.php?lang=${langSelection.value}`
+        })
+    </script>
 </body>
+
 </html>
