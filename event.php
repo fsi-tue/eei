7ffbe0f2e742cacee91b89f9d3be3e4a1896dadf
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2024-11-26 19:51:12 +0100

    Rewrite event and index in a more idiomatic way

--- event.php
+++ event.php
@@ -4,270 +4,293 @@ require_once 'utils.php';
 require_once 'i18n/i18n.php';
 require_once 'event_type.php';
 
-// global variables
 global $i18n, $events, $CONFIG_TERM, $FILE_REVISION, $CONFIG_CONTACT;
 
-// Apply different checks to avoid loading the page, e.g., if the event is not active
-const LOCATION = 'Location:/';
-// If event id is not set, redirect to main page.
-if (!isset($_GET["e"])) {
-	header(LOCATION);
-	die();
+// Redirect to main page if event ID is not provided or invalid.
+const REDIRECT_URL = 'Location:/';
+$eventId = filter_input(INPUT_GET, 'e', FILTER_SANITIZE_ENCODED);
+if (!$eventId || !isset($events[$eventId])) {
+	header(REDIRECT_URL);
+	exit;
 }
 
-// Get event id
-$event_id = filter_input(INPUT_GET, "e", FILTER_SANITIZE_ENCODED);
+$event = $events[$eventId];
+$registrationId = filter_input(INPUT_GET, 'r', FILTER_SANITIZE_ENCODED);
 
-if (!isset($events[$event_id])) {
-	// Redirect to main page
-	header(LOCATION);
-	die();
+// Handle POST request, e.g. unsubscribe or register
+renderPage($event, $registrationId);
+
+/**
+ * Handles the POST request
+ * Example: Unsubscribe from an event or register for an event
+ * @param mixed $event
+ * @param mixed $registrationId
+ * @return void
+ */
+function handlePostRequest($event, $registrationId)
+{
+	global $i18n;
+
+	if ($registrationId) {
+		$result = deleteRegistration($registrationId, $event);
+		renderMessageBlock($result[0], $result[1]);
+	} else {
+		$result = register($event);
+		renderMessageBlock($result[0], $result[1]);
+	}
 }
-/* @var Event $event */
-$event = $events[$event_id];
 
-?>
-<!DOCTYPE html>
-<html lang="<?= $i18n->getLanguage() ?>">
+/**
+ * Renders a message block
+ * Example: Success or error message
+ * @param mixed $isSuccess
+ * @param mixed $message
+ * @return void
+ */
+function renderMessageBlock($isSuccess, $message)
+{
+	$class = $isSuccess ? 'info' : 'error';
+	echo "<div class=\"text-block $class\">$message</div>";
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css<?= $FILE_REVISION; ?>">
-    <link rel="stylesheet" href="css/input.css<?= $FILE_REVISION; ?>">
-    <link rel="stylesheet" href="css/icons.css<?= $FILE_REVISION; ?>">
-    <title><?= $event->name . " - " . $CONFIG_TERM ?></title>
-</head>
+/**
+ * Renders the complete page
+ * Example: Event details, registration form, error messages
+ * @param mixed $event
+ * @param mixed $registrationId
+ * @return void
+ */
+function renderPage($event, $registrationId)
+{
+	global $i18n, $CONFIG_TERM, $FILE_REVISION, $CONFIG_CONTACT;
 
-<body>
-<div id="center" class="small">
-    <h1><?= $event->name . " - " . $CONFIG_TERM ?></h1>
-    <h2 class="description icon clock"><?= $event->getEventDateString() ?></h2>
-    <h2 class="description icon marker"><?= $event->location; ?></h2>
-    <h2 class="description"><?= $i18n['remaining'] . ": " . $event->getRemainingSpots() ?></h2>
-	<?php if($event->dinosAllowed) {?> <h4 class="description"> <?= $i18n['dinos'] ?></h4> <?php } ?>
-	<?= $event->text ?>
-    <br>
+	$language = htmlspecialchars($i18n->getLanguage(), ENT_QUOTES);
+	?>
+	<!DOCTYPE html>
+	<html lang="<?= $language ?>">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="css/style.css<?= $FILE_REVISION; ?>">
+		<link rel="stylesheet" href="css/input.css<?= $FILE_REVISION; ?>">
+		<link rel="stylesheet" href="css/icons.css<?= $FILE_REVISION; ?>">
+		<title><?= htmlspecialchars($event->name) . " - " . $CONFIG_TERM ?></title>
+	</head>
+
+	<body>
+		<div id="center" class="small">
+			<h1><?= htmlspecialchars($event->name) . " - " . $CONFIG_TERM ?></h1>
+			<h2 class="description icon clock"><?= htmlspecialchars($event->getEventDateString()) ?></h2>
+			<h2 class="description icon marker"><?= htmlspecialchars($event->location) ?></h2>
+			<h2 class="description"><?= $i18n['remaining'] . ": " . htmlspecialchars($event->getRemainingSpots()) ?></h2>
+			<?php if ($event->dinosAllowed): ?>
+				<h4 class="description"><?= $i18n['dinos'] ?></h4>
+			<?php endif; ?>
+			<?= $event->text ?>
+			<?php if (!empty($event->info)): ?>
+				<div class="text-block info"><?= $event->info ?></div>
+			<?php endif; ?>
+			<div class="block">
+				<?php
+				if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+					handlePostRequest($event, $registrationId);
+				} else {
+					if ($registrationId) {
+						renderUnsubscribeForm($event, $registrationId);
+					} elseif ($event->canRegister()) {
+						renderRegistrationForm($event);
+					} else {
+						renderErrorMessages($event);
+					}
+				}
+				?>
+				<div class="container">
+					<a href="index.php?lang=<?= $language ?>">
+						<div class="link"><?= $i18n['back'] ?></div>
+					</a>
+					<a href="calendar.php?e=<?= htmlspecialchars($event->link) ?>&lang=<?= $language ?>&ics">
+						<div class="link color-border"><?= $i18n['calendar_download'] ?></div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</body>
+
+	</html>
 	<?php
-	if ($event->info != '') {
-		?>
-        <div class="text-block info"><?= $event->info ?></div>
+}
+
+/**
+ * Renders the unsubscribe form
+ * Example: Unsubscribe from an event
+ * @param mixed $event
+ * @return array
+ */
+function renderUnsubscribeForm($event, $registrationId)
+{
+	global $i18n;
+
+	?>
+	<form action="event.php?e=<?= htmlspecialchars($event->link) ?>&r=<?= htmlspecialchars($registrationId) ?>"
+		method="post">
+		<div class="text-block"><?= $i18n->translate('unsubscribe_text') ?></div>
+		<input type="hidden" name="registration_id" value="<?= htmlspecialchars($registrationId) ?>">
+		<input type="submit" name="delete_registration" value="<?= $i18n->translate('unsubscribe') ?>">
+	</form>
+	<?php
+}
+
+function renderRegistrationForm($event)
+{
+	global $i18n;
+	?>
+	<form method="post" action="#">
+		<label for="form-name"><?= $i18n['form_yourName'] ?>:<br>
+			<input type="text" id="form-name" name="name" required size="30">
+		</label><br><br>
+		<label for="form-mail"><?= $i18n['form_email'] ?>:<br>
+			<input type="email" id="form-mail" name="mail" required size="30">
+		</label><br><br>
 		<?php
-	} ?>
-    <div class="block>">
-		<?php
-
-		// Check if a registration id is given
-		if (isset($_GET["r"])) {
-			$registration_id = filter_input(INPUT_GET, "r", FILTER_SANITIZE_ENCODED);
-		}
-
-		// Check if it is a POST request
-		// POST requests are used to register for an event or to delete a registration
-		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-			//////////////////////////////////////////
-			// REGISTRATION OR UNSUBSCRIPTION PROCESSING
-
-			// Registration id is used to find the registration in the csv file
-		if (isset($registration_id)) {
-			// Delete registration
-			$del_register_ret = deleteRegistration($registration_id, $event);
-			?>
-            <!-- Print the result of the deletion -->
-            <div class="text-block <?= $del_register_ret[0] ? 'info' : 'error' ?>">
-				<?= $del_register_ret[1] ?>
-            </div>
-			<?php
-		} else {
-			// Register for event
-			$register_ret = register($event);
-			?>
-            <div class="text-block <?= $register_ret[0] ? 'info' : 'error' ?>">
-				<?= $register_ret[1] ?>
-            </div>
-		<?php
-		}
-		} else {
-		//////////////////////////////////////////
-		// REGISTRATION AND UNSUBSCRIPTION FORM
-
-		// Check if a registration id is given
-		if (isset($registration_id)) {
+		renderCourseOptions($event);
+		renderFoodOptions($event);
+		renderFoodBreakfastOptions($event);
 		?>
-            <form action="event.php?e=<?= $event->link ?>&r=<?= $registration_id ?>&lang=<?= $i18n->getLanguage() ?>"
-                  method="post">
-                <div class="text-block">
-					<?= $i18n->translate('unsubscribe_text') ?>
-                </div>
-                <input type="hidden" name="registration_id" value="<?= $registration_id ?>">
-                <input type="submit" name="delete_registration" value="<?= $i18n->translate('unsubscribe') ?>">
-            </form>
-		<?php
-		} else {
+		<script src="js/saveFormValues.js"></script>
+		<input type="submit" value="<?= $i18n['send'] ?>" onclick="saveFormValues()">
+	</form>
+	<?php
+}
 
-		// THIS IS NOT A LOOP!
-		// It is used to break out of the loop if an error occurs
-		do {
-		// Check for possible errors and print them if necessary
-		$time = time();
-		if (!$event->canRegister()) {
+function renderCourseOptions($event)
+{
+	if (!$event->form['course_required']) {
+		return;
+	}
+
+	global $i18n;
+
+	echo $i18n['form_study_programme'] . ':<br>';
+	$courses = [
+		['Informatik', 'form_cs'],
+		['Lehramt', 'form_cs_ed'],
+		['Bioinformatik', 'form_cs_bio'],
+		['Medizininformatik', 'form_cs_med'],
+		['Medieninformatik', 'form_cs_media'],
+		['Maschinelles Lernen', 'form_ml'],
+		['Kognitionswissenschaft', 'form_cog'],
+		['Nebenfach', 'form_subsidiary']
+	];
+	foreach ($courses as $course) {
 		?>
-            <div class="text-block error">
-				<?php
-				// These are sorted by priority
-				if ($event->cancelled) {
-					// Event is cancelled
-					// This has obviously the highest priority and should be displayed first
-					echo $i18n->translate('event_cancelled', array('EVENT_NAME' => $event->name, 'EMAIL_CONTACT' => $CONFIG_CONTACT));
-				} elseif ($event->getRemainingSpots() == 0) {
-					// Event is full
-					echo $i18n['event_full'];
-				} elseif (!$event->isUpcoming()) {
-					echo $i18n['event_is_past'];
-				} elseif ($time < $event->getRegistrationStartUTS() && $event->getRegistrationStartUTS() > 0) {
-					// Event is not yet open for registration
-					echo $i18n->translate('start_of_registration', array('REGISTRATION_DATE' => $event->getRegistrationDateString()));
-				} elseif ($time > $event->getRegistrationEndUTS()) {
-					// Event is no longer open for registration
-					echo $i18n['end_of_registration'];
-				}
-				?>
-            </div>
-		<?php break; // Break out of the 'if'-statement because there's an error!
-		}
+		<label>
+			<input type="radio" class="form-studiengang" name="studiengang" value="<?= $course[0] ?>" required>
+			<?= $i18n[$course[1]] ?>
+		</label>
+		<br>
+	<?php } ?>
+	
+	<br><?= $i18n['form_degree'] ?>:<br>
+	<?php
+
+	// Degree
+	$degrees = ['Bachelor', 'Master'];
+	foreach ($degrees as $degree) {
 		?>
+		<label>
+			<input type="radio" class="form-abschluss" name="abschluss" value="<?= $degree ?>" required>
+			<?= $degree ?>
+		</label>
+		<br>
+	<?php } ?>
 
-            <form method="post" action="#">
-                <label for="form-name">
-					<?= $i18n['form_yourName'] ?>:<br>
-                    <input type="text" id="form-name" name="name" required size="30">
-                </label>
-                <br><br>
-
-                <label for="form-mail">
-					<?= $i18n['form_email'] ?>:<br>
-                    <input type="email" id="form-mail" name="mail" required size="30">
-                </label>
-                <br><br>
-				<?php
-
-				// Courses
-				if ($event->form['course_required']) {
-					echo $i18n['form_study_programme'] . ':<br>';
-					$courses = [
-						['Informatik', 'form_cs'],
-						['Lehramt', 'form_cs_ed'],
-						['Bioinformatik', 'form_cs_bio'],
-						['Medizininformatik', 'form_cs_med'],
-						['Medieninformatik', 'form_cs_media'],
-						['Maschinelles Lernen', 'form_ml'],
-						['Kognitionswissenschaft', 'form_cog'],
-						['Nebenfach', 'form_subsidiary']
-					];
-					foreach ($courses as $course) {
-						?>
-                        <label>
-                            <input type="radio" class="form-studiengang" name="studiengang"
-                                   value="<?= $course[0] ?>" required>
-							<?= $i18n[$course[1]] ?>
-                        </label>
-                        <br>
-					<?php } ?>
-
-                    <br><?= $i18n['form_degree'] ?>:<br>
-					<?php
-					// Degree
-					$degrees = ['Bachelor', 'Master'];
-					foreach ($degrees as $degree) {
-						?>
-                        <label>
-                            <input type="radio" class="form-abschluss" name="abschluss"
-                                   value="<?= $degree ?>" required>
-							<?= $degree ?>
-                        </label>
-                        <br>
-					<?php } ?>
-
-                    <br>Semester: <br>
-					<?php
-					// Semester
-					$semesters = ['1', '2', '3', $i18n['form_many']];
-					foreach ($semesters as $semester) {
-						?>
-                        <label>
-                            <input type="radio" class="form-semester" name="semester" value="<?= $semester ?>" required>
-							<?= $semester ?>
-                        </label>
-                        <br>
-					<?php } ?>
-                    <br>
-					<?php
-				}
-
-				// Food
-				if ($event->form['food']) {
-					echo $i18n['form_food'] . ':<br>';
-					$food_preferences = [
-						$i18n['form_food_no_preference'],
-						$i18n['form_food_vegetarian'],
-						$i18n['form_food_vegan'],
-						$i18n['form_food_no_pork']
-					];
-					foreach ($food_preferences as $preference) {
-						?>
-                        <label>
-                            <input type="radio" class="form-essen" name="essen" value="<?= $preference ?>" required>
-							<?= $preference ?>
-                        </label>
-                        <br>
-					<?php }
-				}
-				?>
-                <br>
-
-				<?php
-				// Breakfast
-				if ($event->form['breakfast']) {
-					echo $i18n['form_breakfast'] . ':<br>';
-					$food_preferences = [
-						$i18n['form_food_no_preference'],
-						$i18n['form_food_sweet'],
-						$i18n['form_food_salty'],
-					];
-					foreach ($food_preferences as $preference) {
-						?>
-                        <label>
-                            <input type="radio" class="form-fruehstueck" name="fruestueck" value="<?= $preference ?>"
-                                   required>
-							<?= $preference ?>
-                        </label>
-                        <br>
-					<?php }
-				} ?>
-                <input type="submit" value="<?= $i18n['send'] ?>" onclick="saveFormValues()">
-            </form>
-            <script type="text/javascript" src="js/saveFormValues.js"></script>
-			<?php
-		} while (false); // Close the non-executing loop
-		}
-		}
+	<br>Semester: <br>
+	<?php
+	// Semester
+	$semesters = ['1', '2', '3', $i18n['form_many']];
+	foreach ($semesters as $semester) {
 		?>
-        <div class="container">
-            <a href="index.php?lang=<?= $i18n->getLanguage() ?>">
-                <div class="link">
-					<?= $i18n['back'] ?>
-                </div>
-            </a>
+		<label>
+			<input type="radio" class="form-semester" name="semester" value="<?= $semester ?>" required>
+			<?= $semester ?>
+		</label>
+		<br>
+	<?php } ?>
+	<br>
+	<?php
+}
 
-            <a href="calender.php?e=<?= $event_id ?>&lang=<?= $i18n->getLanguage() ?>&ics">
-                <div class="link color-border">
-					<?= $i18n['calender_download'] ?>
-                </div>
-            </a>
-        </div>
-    </div>
-</div>
-</body>
+function renderFoodOptions($event)
+{
+	if (!$event->form['food']) {
+		return;
+	}
 
-</html>
+	global $i18n;
+
+	echo $i18n['form_food'] . ':<br>';
+	$foodPreferences = [
+		$i18n['form_food_no_preference'],
+		$i18n['form_food_vegetarian'],
+		$i18n['form_food_vegan'],
+		$i18n['form_food_no_pork']
+	];
+	foreach ($foodPreferences as $preference) {
+		?>
+		<label>
+			<input type="radio" class="form-essen" name="essen" value="<?= $preference ?>" required>
+			<?= $preference ?>
+		</label>
+		<br>
+	<?php } ?>
+	<br>
+	<?php
+}
+
+function renderFoodBreakfastOptions($event)
+{
+	if (!$event->form['breakfast']) {
+		return;
+	}
+
+	global $i18n;
+	echo $i18n['form_breakfast'] . ':<br>';
+	$foodPreferences = [
+		$i18n['form_food_no_preference'],
+		$i18n['form_food_sweet'],
+		$i18n['form_food_salty'],
+	];
+
+	foreach ($foodPreferences as $preference) {
+		?>
+		<label>
+			<input type="radio" class="form-fruehstueck" name="fruestueck" value="<?= $preference ?>" required>
+			<?= $preference ?>
+		</label>
+		<br>
+	<?php } ?>
+	<br>
+	<?php
+}
+
+
+function renderErrorMessages($event)
+{
+	global $i18n, $CONFIG_CONTACT;
+
+	echo "<div class=\"text-block error\">";
+	if ($event->cancelled) {
+		echo $i18n->translate('event_cancelled', ['EVENT_NAME' => $event->name, 'EMAIL_CONTACT' => $CONFIG_CONTACT]);
+	} elseif ($event->getRemainingSpots() == 0) {
+		echo $i18n['event_full'];
+	} elseif (!$event->isUpcoming()) {
+		echo $i18n['event_is_past'];
+	} elseif (time() < $event->getRegistrationStartUTS()) {
+		echo $i18n->translate('start_of_registration', ['REGISTRATION_DATE' => $event->getRegistrationDateString()]);
+	} elseif (time() > $event->getRegistrationEndUTS()) {
+		echo $i18n['end_of_registration'];
+	}
+	echo "</div>";
+}
--- index.php
+++ index.php
@@ -6,22 +6,22 @@ require_once 'i18n/i18n.php';
 
 global $i18n, $CONFIG_TERM, $FILE_REVISION, $events;
 
-// Loads the environment variables from the .env file
+// Load environment variables from the .env file
 loadEnv('.env');
 
-// Create eei-registration folder if it does not exist
+// Ensure the eei-registration folder exists
 createEeiRegistrationFolder();
 
-// Sort the data using the custom comparison function
+// Define a function to sort events by their start date
 /**
- * Sorts the events by date
+ * Sorts events chronologically by their start time
  * @param Event $a
  * @param Event $b
- * @return bool
+ * @return int
  */
 function sortByDate(Event $a, Event $b): int
 {
-	return $a->getEventStartUTS() - $b->getEventStartUTS();
+    return $a->getEventStartUTS() - $b->getEventStartUTS();
 }
 
 usort($events, 'sortByDate');
@@ -34,87 +34,77 @@ usort($events, 'sortByDate');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css<?= $FILE_REVISION; ?>">
-    <link rel="stylesheet" href="css/icons.css<?= $FILE_REVISION; ?>">
-    <link rel="stylesheet" href="css/i18n.css<?= $FILE_REVISION; ?>">
+    <link rel="stylesheet" href="css/style.css<?= $FILE_REVISION ?>">
+    <link rel="stylesheet" href="css/icons.css<?= $FILE_REVISION ?>">
+    <link rel="stylesheet" href="css/i18n.css<?= $FILE_REVISION ?>">
     <title><?= $i18n['title'] ?></title>
 </head>
 
 <body>
-<!-- Icons made by fontawesome.com under CC BY 4.0 License https://fontawesome.com/license/free -->
-<!-- The BBQ-Grill Icon is made by Smashicons from www.flaticon.com -->
-<div id="center">
-    <h1><?= $i18n['title'] ?> - <?= $CONFIG_TERM ?></h1>
+    <!-- Icons courtesy of fontawesome.com under CC BY 4.0 License -->
+    <!-- BBQ-Grill icon by Smashicons from flaticon.com -->
+    <div id="center">
+        <h1><?= $i18n['title'] ?> - <?= $CONFIG_TERM ?></h1>
 
-    <div class="container">
-        <label>
-            <div class="language-switcher">
-                <select id="lang-selection" aria-label="Select language">
-                    <option value='de' <?= $i18n->getLanguage() === 'de' ? 'selected' : '' ?>>🇩🇪 Deutsch</option>
-                    <option value="en" <?= $i18n->getLanguage() === 'en' ? 'selected' : '' ?>>🇬🇧 English</option>
-                </select>
-            </div>
-        </label>
-    </div>
+        <div class="container">
+            <label>
+                <div class="language-switcher">
+                    <select id="lang-selection" aria-label="Select language">
+                        <option value="de" <?= $i18n->getLanguage() === 'de' ? 'selected' : '' ?>>🇩🇪 Deutsch</option>
+                        <option value="en" <?= $i18n->getLanguage() === 'en' ? 'selected' : '' ?>>🇬🇧 English</option>
+                    </select>
+                </div>
+            </label>
+        </div>
 
-    <div class="container">
-		<?php
-		// Sort events by date and if they are active
-		$sorted_events = $events;
-		usort($sorted_events, function (Event $a, Event $b) {
-			// 1. upcoming events first
-            // 2. then sort by date
-            if ($a->isPast() && !$b->isPast()) {
-                return 1;
-            } elseif (!$a->isPast() && $b->isPast()) {
-                return -1;
-            } else {
+        <div class="container">
+            <?php
+            // Sort events by activity status and date
+            $sorted_events = $events;
+            usort($sorted_events, function (Event $a, Event $b) {
+                if ($a->isPast() && !$b->isPast()) {
+                    return 1; // Past events appear after active ones
+                } elseif (!$a->isPast() && $b->isPast()) {
+                    return -1;
+                }
                 return $a->getEventStartUTS() - $b->getEventStartUTS();
-            }
-		});
+            });
 
-		// Display the events
-		foreach ($sorted_events as /* @var Event $event */
-				 $event) {
-            ?>
-            <a href="event.php?e=<?= $event->link ?>&lang=<?= $i18n->getLanguage() ?>">
-                <div class="box icon <?= $event->icon ?> <?= $event->isPast() ? ' past ' : '' ?> float-style <?= $event->eventIsTakingPlace() ? 'today' : '' ?>">
-                    <p class="name"><?= $event->name ?></p>
-                    <p class="date"><?= $event->getEventDateString(array('compact' => true)) ?></p>
-                </div>
-            </a>
-			<?php
-		}
-		?>
-    </div>
-    <br>
-    <div class="footnotes">
-        <p>
-			<?php echo $i18n['index_savedDataDisclaimer']; ?>
-        </p>
+            // Render the sorted events
+            foreach ($sorted_events as $event) { ?>
+                <a href="event.php?e=<?= $event->link ?>&lang=<?= $i18n->getLanguage() ?>">
+                    <div class="box icon <?= $event->icon ?> <?= $event->isPast() ? 'past' : '' ?> float-style <?= $event->eventIsTakingPlace() ? 'today' : '' ?>">
+                        <p class="name"><?= $event->name ?></p>
+                        <p class="date"><?= $event->getEventDateString(['compact' => true]) ?></p>
+                    </div>
+                </a>
+            <?php } ?>
+        </div>
+
         <br>
-        <div class="container">
-            <input id="btn-clr" type="submit" value="<?= $i18n['delete'] ?>"
-                   onclick="!localStorage.clear() && alert('<?= $i18n['index_deletedData'] ?>">
-        </div>
-        <div class="container">
-            <a href="https://github.com/fsi-tue/eei">
-                <div class="link color-border">
-                    Source Code auf Github
-                </div>
-            </a>
+        <div class="footnotes">
+            <p><?= $i18n['index_savedDataDisclaimer'] ?></p>
+            <br>
+            <div class="container">
+                <input id="btn-clr" type="submit" value="<?= $i18n['delete'] ?>"
+                       onclick="!localStorage.clear() && alert('<?= $i18n['index_deletedData'] ?>')">
+            </div>
+            <div class="container">
+                <a href="https://github.com/fsi-tue/eei">
+                    <div class="link color-border">
+                        Source Code auf GitHub
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 
-</div>
-
-<script>
-	// change language
-	const langSelection = document.getElementById('lang-selection')
-	langSelection.addEventListener('change', () => {
-		this.location.href = `index.php?lang=${langSelection.value}`
-	})
-</script>
+    <script>
+        // Language change handler
+        document.getElementById('lang-selection').addEventListener('change', function () {
+            location.href = `index.php?lang=${this.value}`;
+        });
+    </script>
 </body>
 
 </html>
