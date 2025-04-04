5d39003b68465e8311e7e776bc2eabc5831a66a2
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-04-04 21:48:34 +0200

    Fix Fatal error     : Uncaught Error: Call to undefined function datefmt_create() in /var/www/html/index.php:65 Stack trace: #0 {main} thrown in     /var/www/html/index.php     on line     65

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ require_once 'head.php';
             <a href="event.php?e=<?= $event->link ?>&lang=<?= $i18n->getLanguage() ?>" class="event-link">
                 <div class="event-row <?= $event->isPast() ? 'past' : '' ?> <?= $event->eventIsTakingPlace() ? 'today' : '' ?>">
                     <div class="event-pill event-date-pill">
-                        <?= htmlspecialchars($date_string) ?>
+                        <?= $event->getEventDateString(['compact' => true, 'no_time' => true]) ?>
                     </div>
                     <div class="event-pill event-info-pill">
                         <span class="event-name"><?= htmlspecialchars($event->name) ?></span>
