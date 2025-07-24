8cb86cc16127fd8f0ac4277fcac3efec1284780d
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-07-24 20:12:03 +0200

    chore: update integrity check and doc

--- check_yml_integrity.py
+++ check_yml_integrity.py
@@ -99,6 +99,7 @@ REQUIRED_SCHEMA = Schema(
             Optional("food"): bool,
             Optional("gender"): bool,
             Optional("course_required"): bool
+            Optional("drinks_alcohol"): bool,
         },
         "icon": And(str, is_icon),
         Optional("metas"): [str],
