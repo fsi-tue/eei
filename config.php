c25375b32fc16e17f084b65dcea83b9be36317c9
Najib <najib.qunoo@student.uni-tuebingen.de>
2025-09-13 17:32:53 +0200

    Updated Term

--- config.php
+++ config.php
@@ -12,8 +12,8 @@ $FILE_REVISION = "?v=" . file_get_contents(__DIR__ . "/.git/refs/heads/master", 
 
 # General Information
 $CONFIG_CONTACT = 'fsi@fsi.uni-tuebingen.de';
-$CONFIG_TERM = 'Sommersemester 25'; # example: 'Wintersemester 2023/24' or 'Sommersemester 2024'
-$CONFIG_TERM_SHORT = 'SS25'; # example: 'WS23_24' or 'SS24'
+$CONFIG_TERM = 'Wintersemester 2025/26'; # example: 'Wintersemester 2023/24' or 'Sommersemester 2024'
+$CONFIG_TERM_SHORT = 'WS25_26'; # example: 'WS23_24' or 'SS24'
 $CONFIG_THEME = 'fsi-fsk'; # possible values: 'fsi-fsk' or 'fsi-only'
 // see https://www.php.net/manual/en/language.constants.predefined.php
 $fp = realpath(__DIR__ . "/../eei-registration/") . "/"; #File Prefix
