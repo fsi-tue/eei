773b3b3087bd8f2a3ccc24c02da56d00c866c05c
Luis-Pascal Mack <luis@lmck.eu>
2026-09-07 12:59:26 +0200

    changing studyprogram form (radio->dropdown)

--- event.php
+++ event.php
@@ -247,6 +247,7 @@ function renderCourseOptions($event): void
 
 	global $i18n;
 
+	// Program
 	echo $i18n['form_study_programme'] . ':<br>';
 	$courses = [
 		['Informatik', 'form_cs'],
@@ -261,15 +262,19 @@ function renderCourseOptions($event): void
 		['Kognitionswissenschaft', 'form_cog'],
 		['Nebenfach', 'form_subsidiary']
 	];
-	foreach ($courses as $course) {
-		?>
-        <label>
-            <input type="radio" class="form-studiengang" name="studiengang" value="<?= $course[0] ?>" required>
-			<?= $i18n[$course[1]] ?>
-        </label>
-        <br>
-		<?php
-	} ?>
+	?>
+		<select class="form-studiengang" name="studiengang" required>
+			<option value="" disabled selected></option>
+			<?php 
+			foreach ($courses as $course) {
+				?>
+				<option value="<?= $course[0] ?>">
+					<?= $i18n[$course[1]] ?>
+				</option>
+			<?php 
+			} ?>
+		</select>
+	<br>
 
     <br><?= $i18n['form_degree'] ?>:<br>
 	<?php
